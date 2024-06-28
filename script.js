1a7a654ffc9af51bcb0fbb4c7653dc43b6a0faaa
montelzek <patrykmatras378@gmail.com>
2024-06-28 23:29:32 +0200

    Created a 16x16 grid of divs

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector("#container");
+
+for (let i = 1; i <= 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let i = 1; i <= 16; i++) {
+        const oner = document.createElement("div");
+        oner.classList.add("oner");
+        row.appendChild(oner);
+    }
+}
